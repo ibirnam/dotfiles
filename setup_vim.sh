594d2ae78f454892176abce992ffbda2be59e7e3
Matt Carrier <icecreammatt@gmail.com>
2014-08-07 23:45:11 -0700

    Testing slim install with Docker

--- setup_vim.sh
+++ setup_vim.sh
@@ -4,10 +4,3 @@ mkdir ~/.vim/bundle -p
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 vim +PluginInstall +qall
-
-if [ -d ~/.vim/bundle/YouCompleteMe/ ]
-then
-    cd ~/.vim/bundle/YouCompleteMe/
-    source install.sh
-    cd ~
-fi
